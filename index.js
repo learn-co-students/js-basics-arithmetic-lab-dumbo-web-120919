dee83522a742e1efc61b14341e5b561396cce561
Radouane Bahi <radbahi@gmail.com>
2020-01-26 18:43:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here
+let var1 = 1;
+
+let var2 = 62;
+
+let multiply = var1 * var2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+;
+
+let mod = 10 % 6;
+
+let max = Math.max(1, 2, 3, 4, 20);
