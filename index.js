9be5d18eaae399168c1696c44a301f574a800602
David Cha <davidcha17@yahoo.com>
2020-01-27 11:38:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+const var1 = 31
+const var2 = 2
+
+const multiply = var1 * var2;
+
+const random = Math.random() + 1;
+
+const mod = 4 % 6; 
+
+const max = Math.max(1, 3, 5, 6, 12, 20);
